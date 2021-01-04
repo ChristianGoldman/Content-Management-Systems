471ab6c53461649793a75e45c0aede49f941c0a5
Christian Goldman <christian.goldman9638@gmail.com>
2021-01-03 20:08:50 -0600

    adding delete employee functionality

--- app.js
+++ app.js
@@ -23,16 +23,13 @@ function questions() {
         message: "What would you like to do?",
         choices: [
           "View All Employees",
-          "View All Employees by Department",
-          "View All Employees by Manager",
+          "View All Roles",
+          "View All Departments",
           "Add Employee",
           "Add Department",
           "Add Role",
           "Remove Employee",
-          "Update Employee",
           "Update Employee Role",
-          "View All Roles",
-          "View All Departments",
         ],
       },
     ])
@@ -60,6 +57,10 @@ function questions() {
 
         case "View All Departments":
           viewDepartment();
+          break;
+
+        case "Remove Employee":
+          removeEmployee();
           break;
         default:
       };
@@ -174,5 +175,49 @@ function viewRoles() {
   });
 };
 
+function removeEmployee() {
+  let employeeArray = [];
+  connection.query("SELECT first_name, last_name, role_id, concat(first_name, ' ', last_name, ' ', 'Role ID ', role_id) AS employee FROM employee", function (error, results, fields) {
+    if (error) throw error;
+    for(let i = 0; i < results.length; i++){
+      employeeArray.push(results[i].employee);
+    };
+    inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "deleted",
+        message: "What employee do you want to remove?",
+        choices: employeeArray
+      },
+      {
+        type: "list",
+        name: "confirm",
+        message: "Are you sure you wish to remove this employee?",
+        choices: ["Yes", "No"]
+      },
+    ])
+    .then(function(res) {
+      if(res.confirm === "Yes"){
+        let removedEmp;
+        for (i=0; i < results.length; i++){
+          if (res.deleted === results[i].employee){
+              removedEmp = results[i].employee;
+            };
+            console.log(removedEmp);
+        };
+        connection.query(`DELETE FROM employee WHERE id=${removedEmp};`, function(err, res) {
+          if(err) return err;
+          console.log(`\n EMPLOYEE '${res.employee}' DELETED...\n `);
+        });
+        askAgain();
+      } else {
+        console.log('Employee was not removed!');
+        askAgain();
+      };
+
+    });
+  });
+}
 
 questions();
